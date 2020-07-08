df2b40b773947af6fb756cd39a49057a392b85cd
pavelfromua <50228485+pavelfromua@users.noreply.github.com>
2020-07-08 22:17:12 +0300

    t commit

--- src/main/java/FootballMatch.java
+++ src/main/java/FootballMatch.java
@@ -14,7 +14,7 @@ public class FootballMatch {
     public int matchResult(int result1, int result2, int bet1, int bet2) {
         //int result = (bet1 >= result1 && bet2 >= result2) ? (bet1 == result1 && bet2 == result2) ? 2 : (result1 == result2) ? (bet1 == bet2) ? 1 : 0: (bet1 == bet2) ? 0 : 1 : 0;
         int result = (bet1 == result1 && bet2 == result2) ? 2 : ((bet1 - result1) < 0 || (bet2 - result2) < 0) ? 0: 1;
-
+            //sdfsd
         return result;
     }
 }
