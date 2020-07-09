088c0ce33c2d145902754b78d8a658db6d1c7b3f
pavelfromua <50228485+pavelfromua@users.noreply.github.com>
2020-07-09 09:17:38 +0300

    result4

--- src/main/java/FootballMatch.java
+++ src/main/java/FootballMatch.java
@@ -1,8 +1,8 @@
 public class FootballMatch {
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        return (bet1 == result1 && bet2 == result2) ? 2 : ((bet1 == bet2 && result1 == result2) ||
-               ((((bet2 - bet1) >= 0 && (result2 - result1) >= 0) ||
-               ((bet2 - bet1) < 0 && (result2 - result1) < 0))
+        return (bet1 == result1 && bet2 == result2) ? 2 : ((bet1 == bet2 && result1 == result2)
+                || ((((bet2 - bet1) >= 0 && (result2 - result1) >= 0)
+                || ((bet2 - bet1) < 0 && (result2 - result1) < 0))
                && (result1 != result2 && bet1 != bet2))) ? 1 : 0;
     }
 }
